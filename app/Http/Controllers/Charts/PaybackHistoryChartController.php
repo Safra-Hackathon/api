14aeb088fbc21f9b1d6e3ffcba1b9419769ff6b4
eduardo <eduardo@logcomex.com>
2020-09-13 03:46:42 -0300

    fix: adicionada projeção temporária enquanto não sobe o microserviço que projeta os valores

--- app/Http/Controllers/Charts/PaybackHistoryChartController.php
+++ app/Http/Controllers/Charts/PaybackHistoryChartController.php
@@ -4,11 +4,13 @@ namespace App\Http\Controllers\Charts;
 
 use App\Entities\PaybackTransactionsHistory;
 use App\Http\Controllers\Controller;
+use Carbon\Carbon;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 
 class PaybackHistoryChartController extends Controller
 {
+    protected $projectionDate;
     /**
      * Update Profile
      *
@@ -20,6 +22,12 @@ class PaybackHistoryChartController extends Controller
     public function history(string $start, string $end)
     {
         $userId = Auth::user()->id;
+        $currentDate = substr(Carbon::now()->toISOString(), 0, 10);
+        if ($end > $currentDate) {
+            $this->projectionDate = $end;
+            $end = $currentDate;
+        }
+
         $paybackHistory = PaybackTransactionsHistory::select(
                 DB::raw("LEFT(date::TEXT, 7) as year_month"),
                 DB::raw('MAX(payback_total) as payback')
@@ -30,15 +38,59 @@ class PaybackHistoryChartController extends Controller
             ->get()
             ->sortBy('year_month')
             ->map(function ($item) {
+                $key = $item->year_month . '-01';
                 return collect([
-                    $item->year_month => $item->payback
+                   $key  => $item->payback
                 ]);
             })
             ->reverse()
             ->toArray();
-
         $paybackHistory = array_values($paybackHistory);
 
-        return response()->json($paybackHistory);
+        $data = ['historico' => $paybackHistory];
+
+        if ($this->projectionDate) {
+            // TODO:
+            // envia o payback histórico p/ o microserviço de projeção
+            // recebe as projeções dos próximos x meses
+            // coloca as projeções no array e retorna
+            $paybackHistory = array_reverse($paybackHistory);
+            $lastHistoryElement = array_pop($paybackHistory);
+            $historyLen = count($paybackHistory) - 1;
+
+            if ($historyLen > 1) {
+                $beforeLast = (double) array_values($paybackHistory[$historyLen - 1])[0];
+                $last = (double) array_values($paybackHistory[$historyLen])[0];
+                $lastDateStr = array_key_last($paybackHistory[$historyLen]);
+                $lastDate = Carbon::createFromFormat('Y-m-d', $lastDateStr);
+
+                $rate = $last/$beforeLast;
+
+                $lastDate->addMonth();
+                $last *= $rate;
+                $lastDateStr = substr($lastDate->toISOString(), 0, 10);
+                $data['projetado'][] = [
+                    $lastDateStr => number_format($last, 2)
+                ];
+
+                $lastDate->addMonth();
+                $last *= $rate;
+                $lastDateStr = substr($lastDate->toISOString(), 0, 10);
+                $data['projetado'][] = [
+                    $lastDateStr => number_format($last, 2)
+                ];
+
+                $lastDate->addMonth();
+                $last *= $rate;
+                $lastDateStr = substr($lastDate->toISOString(), 0, 10);
+                $data['projetado'][] = [
+                    $lastDateStr => number_format($last, 2)
+                ];
+            }
+
+            $paybackHistory['historico'][] = $lastHistoryElement;
+        }
+
+        return response()->json($data);
     }
 }
