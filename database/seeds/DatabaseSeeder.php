26bf0161ea47d41faf630903fa7a73f791a955d4
eduardo <eduardo@logcomex.com>
2020-09-13 01:56:54 -0300

    feat: habilita os seeders criados

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(UsersTableSeeder::class);
+        $this->call(UserPaybacksTableSeeder::class);
+        $this->call(PaybackTransactionsHistoryTableSeeder::class);
     }
 }
